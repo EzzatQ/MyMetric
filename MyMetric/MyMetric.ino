5ae4230da9a0877cc8829591ec26c75b2a6dec8b
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2022-02-04 20:23:44 +0200

    initial commit

---
+++ MyMetric/MyMetric.ino
@@ -0,0 +1,49 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <ESPmDNS.h>
+
+#include <AutoConnect.h>
+
+
+const char* MY_METRIC = "MyMetric";
+
+WebServer Server;          
+AutoConnect Portal(Server); //https://hieromon.github.io/AutoConnect
+AutoConnectConfig ACConfig = AutoConnectConfig(MY_METRIC, nullptr);
+
+
+
+
+void rootPage() {
+  char content[] = "Hello, world";
+  Server.send(200, "text/plain", content);
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  Server.on("/", rootPage);
+  ACConfig.title = MY_METRIC;   
+  ACConfig.portalTimeout = 60000;  
+  ACConfig.autoReconnect = true; 
+  Portal.config(ACConfig);
+  if (Portal.begin()) {
+    Serial.println("WiFi connected: " + WiFi.localIP().toString());
+    if (MDNS.begin("mymetric")) {
+      Serial.println("mDNS Configured: http://mymetric.local/");
+      MDNS.addService("http", "tcp", 80);
+    }
+  }
+}
+
+void loop() {
+
+    if (WiFi.status() == WL_CONNECTED) {
+      // Some sketch code for the connected scene is here.
+    }
+    else {
+      // Some sketch code for not connected scene is here.
+    }
+    Portal.handleClient();
+}
